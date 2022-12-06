e6eaa04595c82cee6ac77e222e3795221140be57
trigold <422520625@qq.com>
2022-12-06 17:39:01 +0800

    init

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -7,10 +7,16 @@ use Trigold\Http\Contracts\HttpClient;
 
 class ClientFactory
 {
+    const GUZZLE = 'guzzle';
+
     /**
+     * @param  array   $config
+     * @param  string  $driver
+     *
+     * @return HttpClient
      * @throws Exception
      */
-    public static function create(string $driver = 'guzzle', array $config = []): HttpClient
+    public static function create( array $config = [],string $driver = 'guzzle'): HttpClient
     {
         $driver = ucfirst($driver);
         $class = "\\Trigold\\Http\\Client\\{$driver}";
