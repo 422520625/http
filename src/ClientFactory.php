5e1cb5c9c7d78ad83f063b36bea08036eb9f446e
trigold <422520625@qq.com>
2022-12-06 17:09:20 +0800

    init

---
+++ src/ClientFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Trigold\Http;
+
+use Trigold\Http\Contracts\HttpClient;
+
+class ClientFactory
+{
+    public static function create(string $driver = 'guzzle', array $config = []): HttpClient
+    {
+        $driver = ucfirst($driver);
+        $class = "\\Trigold\\Http\\Client\\{$driver}";
+        if (!class_exists($class)) {
+            throw new \Exception("{$class} not found");
+        }
+        return new $class($config['base_uri'] ?? '', $config['timeout'] ?? 2, $config['options'] ?? []);
+    }
+}
